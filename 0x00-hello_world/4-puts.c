969a003dbce1da6da0922e0f2b7ca2b6643f9c54
Fitsum-Berhane <f.berhaile@gmail.com>
2023-08-11 13:09:50 +0300

    This is a c hello world, written using puts.

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,11 +1,11 @@
 #include <stdio.h>
 /**
- * main 
- *
- * Return: 0
+ * main: void
+ * return: 0
  */
+
 int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle\n");
-	return(0);
+	return (0);
 }
