6c26c81dc4c795c7a3a7ff311dd7c68d0632bbe7
Fitsum-Berhane <f.berhaile@gmail.com>
2023-08-14 16:48:17 +0300

    first trial

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+/**
+ * Description - prints numbers in single digit
+ * main - Entry point
+ *
+ * Return: Always 0 (success)
+ */
+
+int main(void)
+{
+	for (char a = 0; a < 10; a++)
+		putchar(a);
+	return (0);
+}
