0527c582c258329a4b3759904c5474f5e8b1adec
Fitsum-Berhane <f.berhaile@gmail.com>
2023-08-15 21:37:50 +0300

    still very confused

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#define _putchar putchar
+/**
+ * print_alphabet_x10 - prints alphabets 10 times
+ * Return: Always 0.
+ */
+
+void print_alphabet_x10(void)
+{
+	char letter;
+	int i;
+
+	for (i = 0; i < 10; i++)
+	{
+		for (letter - 'a'; letter <= 'z'; letter++)
+			_putchar(letter);
+		_putchar('\n');
+	}
+}
