f0b11e5adf3c203819492a07e07f87869d835946
Fitsum-Berhane <f.berhaile@gmail.com>
2023-08-15 18:02:16 +0300

    prints alphabet

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,27 +1,12 @@
 #include "main.h"
 /**
-* main - check the code
-*
-* Return: Always 0.
+* print_alphabet - print all alphabet in lowercase
 */
-
-void print_alphabet(void);
-int main(void)
-{
-	print_alphabet();
-	return (0);
-}
-
-/**
- * print_alphabet - prints alphabets
- *
- * Return: Always 0.
- */
-
 void print_alphabet(void)
 {
-	int letter;
-
+	char letter;
 	for (letter = 'a'; letter <= 'z'; letter++)
-		putchar(letter);
+		_putchar(letter);
+	_putchar('\n');
 }
+
