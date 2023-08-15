86a289f860b9210dffe54bbc3c5bb6f1d185eb24
Fitsum-Berhane <f.berhaile@gmail.com>
2023-08-15 16:59:14 +0300

    change

---
+++ 0x02-functions_nested_loops/_putchar.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+/**
+ * program - prints _putchar
+ *
+ * Return: always 0 (success)
+ */
+
+void program(void)
+{
+	printf("_putchar\n");
+}
+
+/**
+* main - calls functin program
+*
+* Return: always 0 (success)
+*/
+
+
+int main(void)
+{
+	program();
+	return (0);
+}
