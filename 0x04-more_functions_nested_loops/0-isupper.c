aeb37dfe3f974aed37c2849afcf7a4f91477a684
Fitsum-Berhane <f.berhaile@gmail.com>
2023-08-18 09:37:20 +0300

    Hope this works now

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -1,14 +1,8 @@
-#include "main.h"
-/**
- * _isupper - checks if the character is capital
- *
- * Reference: Always 0.
- */
-
 int _isupper(char c)
 {
 	if ( char c >= 'a' && char c <= 'z')
 		return 0;
 	else if ( char c>= 'A' && char c <= 'Z' )
 		return 1;
+	return (0);
 }
