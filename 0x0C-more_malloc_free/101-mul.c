be4984f0cdf69d2e7dda03b53c8c5cc2188a5f4f
Fitsum-Berhane <f.berhaile@gmail.com>
2023-09-07 14:55:42 +0300

    additional task

--- 0x0C-more_malloc_free/101-mul.c
+++ 0x0C-more_malloc_free/101-mul.c
@@ -13,17 +13,14 @@ void multiply(char *num1, char *num2);
  */
 int main(int argc, char *argv[])
 {
-    if (argc != 3 || !_isdigit(argv[1]) || !_isdigit(argv[2]))
-    {
-        printf("Error\n");
-        return 98;
-    }
-
-    multiply(argv[1], argv[2]);
-
-    return 0;
+	if (argc != 3 || !_isdigit(argv[1]) || !_isdigit(argv[2]))
+	{
+		printf("Error\n");
+		return (98);
+	}
+	multiply(argv[1], argv[2]);
+	return (0);
 }
-
 /**
  * _isdigit - Checks if a string contains only digits
  * @str: String to check
@@ -32,18 +29,16 @@ int main(int argc, char *argv[])
  */
 int _isdigit(char *str)
 {
-    int i = 0;
+	int i = 0;
 
-    while (str[i])
-    {
-        if (str[i] < '0' || str[i] > '9')
-            return 0;
-        i++;
-    }
-
-    return 1;
+	while (str[i])
+	{
+		if (str[i] < '0' || str[i] > '9')
+			return (0);
+		i++;
+	}
+	return (1);
 }
-
 /**
  * multiply - Multiplies two positive numbers
  * @num1: First number as string
@@ -51,48 +46,40 @@ int _isdigit(char *str)
  */
 void multiply(char *num1, char *num2)
 {
-    int len1 = 0, len2 = 0, i, j, carry, n1, n2, sum;
-    int *result;
+	int len1 = 0, len2 = 0, i, j, carry, n1, n2, sum;
+	int *result;
 
-    while (num1[len1])
-        len1++;
-    while (num2[len2])
-        len2++;
-
-    result = malloc(sizeof(int) * (len1 + len2));
-    if (result == NULL)
-    {
-        printf("Error\n");
-        exit(98);
-    }
-
-    for (i = 0; i < (len1 + len2); i++)
-        result[i] = 0;
-
-    for (i = len1 - 1; i >= 0; i--)
-    {
-        carry = 0;
-        n1 = num1[i] - '0';
-
-        for (j = len2 - 1; j >= 0; j--)
-        {
-            n2 = num2[j] - '0';
-            sum = (n1 * n2) + result[i + j + 1] + carry;
-            carry = sum / 10;
-            result[i + j + 1] = sum % 10;
-        }
-
-        if (carry > 0)
-            result[i + j + 1] += carry;
-    }
-
-    i = 0;
-    while (result[i] == 0 && i < (len1 + len2 - 1))
-        i++;
-
-    for (; i < (len1 + len2); i++)
-        printf("%d", result[i]);
-    printf("\n");
-
-    free(result);
+	while (num1[len1])
+		len1++;
+	while (num2[len2])
+		len2++;
+	result = malloc(sizeof(int) * (len1 + len2));
+	if (result == NULL)
+	{
+		printf("Error\n");
+		exit(98);
+	}
+	for (i = 0; i < (len1 + len2); i++)
+		result[i] = 0;
+	for (i = len1 - 1; i >= 0; i--)
+	{
+		carry = 0;
+		n1 = num1[i] - '0';
+		for (j = len2 - 1; j >= 0; j--)
+		{
+			n2 = num2[j] - '0';
+			sum = (n1 * n2) + result[i + j + 1] + carry;
+			carry = sum / 10;
+			result[i + j + 1] = sum % 10;
+		}
+		if (carry > 0)
+			result[i + j + 1] += carry;
+	}
+	i = 0;
+	while (result[i] == 0 && i < (len1 + len2 - 1))
+		i++;
+	for (; i < (len1 + len2); i++)
+		printf("%d", result[i]);
+	printf("\n");
+	free(result);
 }
