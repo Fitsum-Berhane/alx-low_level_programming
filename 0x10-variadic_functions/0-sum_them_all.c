ebe9149a48872e6068c0ebcdc828d68031689e1b
Fitsum-Berhane <f.berhaile@gmail.com>
2023-09-14 11:42:47 +0300

    a function that returns the sum of all its parameters.

---
+++ 0x10-variadic_functions/0-sum_them_all.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <stdarg.h>
+/**
+ * sum_them_all - sums all the given numbers
+ * @n: number of arguments
+ * Return: int
+ */
+int sum_them_all(const unsigned int n, ...)
+{
+	va_list list;
+	unsigned int sum = 0, i = 0;
+
+	va_start(list, n);
+	for (i = 0; i < n; i++)
+		sum += va_arg(list, unsigned int);
+	va_end (list);
+
+	return (sum);
+}
