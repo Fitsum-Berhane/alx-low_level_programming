ec2d4007f5164522ee5a24f8bb597a017de36b32
Fitsum-Berhane <f.berhaile@gmail.com>
2023-09-21 14:08:03 +0300

    changes

--- 0x12-singly_linked_lists/1-list_len.c
+++ 0x12-singly_linked_lists/1-list_len.c
@@ -1,12 +1,12 @@
 #include <stdlib.h>
 #include "lists.h"
-
 /**
- * list_len - returns the number of elements in a linked list
- * @h: pointer to the list_t list
+ * list_len - Returns the number of elements in a linked list.
+ * @h: Pointer to the list_t list.
  *
- * Return: number of elements in h
+ * Return: The number of elements in the linked list.
  */
+
 size_t list_len(const list_t *h)
 {
 	size_t n = 0;
