0dd3a3e4838a89aed2afbc77b640adaee6a6730a
Fitsum-Berhane <f.berhaile@gmail.com>
2023-09-25 19:35:16 +0300

    changes and betty corrections

--- 0x13-more_singly_linked_lists/10-delete_nodeint.c
+++ 0x13-more_singly_linked_lists/10-delete_nodeint.c
@@ -2,7 +2,7 @@
 #include "lists.h"
 
 /**
- * delete_nodeint_at_index - Deletes the node at index index of a listint_t linked list.
+ * delete_nodeint_at_index - Deletes the node at index.
  * @head: Pointer to a pointer to the head node of the list.
  * @index: Index of the node that should be deleted. Index starts at 0.
  *
@@ -10,35 +10,29 @@
  */
 int delete_nodeint_at_index(listint_t **head, unsigned int index)
 {
-    listint_t *current, *temp;
-    unsigned int i;
+	listint_t *current, *temp;
+	unsigned int i;
 
-    if (head == NULL || *head == NULL)
-        return -1;
-
-    if (index == 0)
-    {
-        temp = *head;
-        *head = (*head)->next;
-        free(temp);
-        return 1;
-    }
-
-    current = *head;
-    for (i = 0; i < index - 1; i++)
-    {
-        if (current->next == NULL)
-            return -1;
-
-        current = current->next;
-    }
-
-    temp = current->next;
-    if (temp == NULL)
-        return -1;
-
-    current->next = temp->next;
-    free(temp);
-
-    return 1;
+	if (head == NULL || *head == NULL)
+		return (-1);
+	if (index == 0)
+	{
+		temp = *head;
+		*head = (*head)->next;
+		free(temp);
+		return (1);
+	}
+	current = *head;
+	for (i = 0; i < index - 1; i++)
+	{
+		if (current->next == NULL)
+			return (-1);
+		current = current->next;
+	}
+	temp = current->next;
+	if (temp == NULL)
+		return (-1);
+	current->next = temp->next;
+	free(temp);
+	return (1);
 }
--- 0x13-more_singly_linked_lists/8-sum_listint.c
+++ 0x13-more_singly_linked_lists/8-sum_listint.c
@@ -1,7 +1,6 @@
 #include "lists.h"
-
 /**
- * sum_listint - Returns the sum of all the data (n) of a listint_t linked list.
+ * sum_listint - Returns the sum of all the data(n) of a listint_t linked list.
  * @head: Pointer to the head node of the list.
  *
  * Return: The sum of all the data (n) of the list.
@@ -9,14 +8,13 @@
  */
 int sum_listint(listint_t *head)
 {
-    listint_t *current = head;
-    int sum = 0;
+	listint_t *current = head;
+	int sum = 0;
 
-    while (current != NULL)
-    {
-        sum += current->n;
-        current = current->next;
-    }
-
-    return sum;
+	while (current != NULL)
+	{
+		sum += current->n;
+		current = current->next;
+	}
+	return (sum);
 }
