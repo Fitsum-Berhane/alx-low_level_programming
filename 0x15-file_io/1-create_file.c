cb7f3cc4b3397bfd03ca157c13f114e10216b3fe
Fitsum Berhane <f.berhaile@gmail.com>
2023-11-27 18:36:54 +0200

    task 1

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -1,38 +1,43 @@
+#include <fcntl.h>
+#include <unistd.h>
+#include <stdlib.h>
 #include "main.h"
 
 /**
- * create_file - creates a file
- * @filename: filename.
- * @text_content: content writed in the file.
+ * create_file - Creates a file and writes text content to it.
+ * @filename: The name of the file.
+ * @text_content: The text content to write.
  *
- * Return: 1 if it success. -1 if it fails.
+ * Return: 1 on success, -1 on failure.
  */
 int create_file(const char *filename, char *text_content)
 {
-	int fd;
-	int nletters;
-	int rwr;
+	int fd, num_chars, text_len;
 
-	if (!filename)
+	if (filename == NULL)
 		return (-1);
 
-	fd = open(filename, O_CREAT | O_WRONLY | O_TRUNC, 0600);
-
+	fd = open(filename, O_WRONLY | O_CREAT | O_TRUNC, 0600);
 	if (fd == -1)
 		return (-1);
 
-	if (!text_content)
-		text_content = "";
+	if (text_content == NULL)
+	{
+		close(fd);
+		return (1);
+	}
 
-	for (nletters = 0; text_content[nletters]; nletters++)
-		;
+	text_len = 0;
+	while (text_content[text_len] != '\0')
+		text_len++;
 
-	rwr = write(fd, text_content, nletters);
-
-	if (rwr == -1)
+	num_chars = write(fd, text_content, text_len);
+	if (num_chars == -1 || num_chars != text_len)
+	{
+		close(fd);
 		return (-1);
+	}
 
 	close(fd);
-
 	return (1);
 }
